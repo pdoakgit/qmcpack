9c35d3211a670fe169a6cc8d142f55da854a9ea2
Peter Doak <doakpw@ornl.gov>
2020-01-08 18:47:41 -0500

    Many things are dependent on global enum in Walker.h fixing

--- src/Particle/Walker.h
+++ src/Particle/Walker.h
@@ -22,10 +22,11 @@
 #define QMCPLUSPLUS_WALKER_H
 
 #include "OhmmsPETE/OhmmsMatrix.h"
-#include "OhmmsPETE/OhmmsConstantSizeVector.h"
+#include "MinimalContainers/ConstantSizeVector.hpp"
 //#include "OhmmsPETE/ProtectedMatrix.h"
 #include "Utilities/PooledData.h"
 #include "Utilities/PooledMemory.h"
+#include "QMCDrivers/WalkerProperties.h"
 #ifdef QMC_CUDA
 #include "type_traits/CUDATypes.h"
 #include "Utilities/PointerPool.h"
@@ -35,29 +36,6 @@
 #include <deque>
 namespace qmcplusplus
 {
-/** an enum denoting index of physical properties
- *
- * LOCALPOTENTIAL should be always the last enumeation
- * When a new enum is needed, modify ParticleSet::initPropertyList to match the list
- */
-enum
-{
-  LOGPSI = 0,      /*!< log(std::abs(psi)) instead of square of the many-body wavefunction \f$|\Psi|^2\f$ */
-  SIGN,            /*!< value of the many-body wavefunction \f$\Psi(\{R\})\f$ */
-  UMBRELLAWEIGHT,  /*!< sum of wavefunction ratios for multiple H and Psi */
-  R2ACCEPTED,      /*!< r^2 for accepted moves */
-  R2PROPOSED,      /*!< r^2 for proposed moves */
-  DRIFTSCALE,      /*!< scaling value for the drift */
-  ALTERNATEENERGY, /*!< alternatelocal energy, the sum of all the components */
-  LOCALENERGY,     /*!< local energy, the sum of all the components */
-  LOCALPOTENTIAL,  /*!< local potential energy = local energy - kinetic energy */
-  PARTICLE_1,      // Walker properties get particle properties assigned eventuall and they are bigger
-  PARTICLE_2,      // ditto since apparently hamiltonians insert these this enum is a terrible idea
-  PARTICLE_3,      // ditto since apparently hamiltonians insert these this enum is a terrible idea
-  PARTICLE_4,      // ditto since apparently hamiltonians insert these this enum is a terrible idea
-  PARTICLE_5,      // ditto since apparently hamiltonians insert these this enum is a terrible idea
-  NUMPROPERTIES = 64    /*!< the number of properties */
-};
 
 /** A container class to represent a walker.
  *
@@ -69,12 +47,17 @@ enum
  * - Age : generation after a move is accepted.
  * - Weight : weight to take the ensemble averages
  * - Multiplicity : multiplicity for branching. Probably can be removed.
- * - Properties  : 2D container. RealTypehe first index corresponds to the H/Psi index and second index >=NUMPROPERTIES.
- * - DataSet : anonymous container.
+ * - Properties  : 2D container. RealType first index corresponds to the H/Psi index and second index >=NUMPROPERTIES.
+ * - DataSet : a contiguous buffer providing a state snapshot of most/all walker data. 
+     Much complicated state management arises in keeping this up to date, 
+     or purposefully out of sync with actual datamembers. Here and in TWF, HAMs and PARTICLE sets
+     associated with the walker.
  */
 template<typename t_traits, typename p_traits>
 struct Walker
 {
+
+  using WP = WalkerProperties::Indexes;
   enum
   {
     DIM = t_traits::DIM
@@ -190,7 +173,7 @@ struct Walker
 #endif
 
   ///create a walker for n-particles
-  inline explicit Walker(int nptcl = 0) : Properties(0, NUMPROPERTIES)
+  inline explicit Walker(int nptcl = 0) : Properties(0, WP::NUMPROPERTIES)
 #ifdef QMC_CUDA
         cuda_DataSet("Walker::walker_buffer"),
         R_GPU("Walker::R_GPU"),
@@ -347,23 +330,23 @@ struct Walker
   {
     Age = 0;
     //Weight=1.0;
-    Properties(LOGPSI)      = logpsi;
-    Properties(SIGN)        = sigN;
-    Properties(LOCALENERGY) = ene;
+    Properties(WP::LOGPSI)      = logpsi;
+    Properties(WP::SIGN)        = sigN;
+    Properties(WP::LOCALENERGY) = ene;
   }
 
   inline void resetReleasedNodeProperty(FullPrecRealType localenergy,
                                         FullPrecRealType alternateEnergy,
                                         FullPrecRealType altR)
   {
-    Properties(ALTERNATEENERGY) = alternateEnergy;
-    Properties(LOCALENERGY)     = localenergy;
-    Properties(SIGN)            = altR;
+    Properties(WP::ALTERNATEENERGY) = alternateEnergy;
+    Properties(WP::LOCALENERGY)     = localenergy;
+    Properties(WP::SIGN)            = altR;
   }
   inline void resetReleasedNodeProperty(FullPrecRealType localenergy, FullPrecRealType alternateEnergy)
   {
-    Properties(ALTERNATEENERGY) = alternateEnergy;
-    Properties(LOCALENERGY)     = localenergy;
+    Properties(WP::ALTERNATEENERGY) = alternateEnergy;
+    Properties(WP::LOCALENERGY)     = localenergy;
   }
   /** reset the property of a walker
    * @param logpsi \f$\log |\Psi|\f$
@@ -384,12 +367,12 @@ struct Walker
                             FullPrecRealType vq)
   {
     Age                     = 0;
-    Properties(LOGPSI)      = logpsi;
-    Properties(SIGN)        = sigN;
-    Properties(LOCALENERGY) = ene;
-    Properties(R2ACCEPTED)  = r2a;
-    Properties(R2PROPOSED)  = r2p;
-    Properties(DRIFTSCALE)  = vq;
+    Properties(WP::LOGPSI)      = logpsi;
+    Properties(WP::SIGN)        = sigN;
+    Properties(WP::LOCALENERGY) = ene;
+    Properties(WP::R2ACCEPTED)  = r2a;
+    Properties(WP::R2PROPOSED)  = r2p;
+    Properties(WP::DRIFTSCALE)  = vq;
   }
 
   /** marked to die
